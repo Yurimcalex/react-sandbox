f30bf24c990f1935471be4e4758ce81d667447bc
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-29 13:55:41 +0300

    Create CommonGreeting component

---
+++ src/qs/CommonGreeting.jsx
@@ -0,0 +1,8 @@
+export default function CommonGreeting() {
+	return (
+		<div className="common-greeting">
+			<h2>Hello! Come on in!</h2>
+			<h3>We are so glad to see you here!</h3>
+		</div>
+	);
+}
--- src/qs/QS.jsx
+++ src/qs/QS.jsx
@@ -1,10 +1,13 @@
+import './QS.css';
 import MyButton from './MyButton.jsx';
+import CommonGreeting from './CommonGreeting.jsx';
 
 export default function QS() {
 	return (
 		<div>
 			<h1>What a cool app!</h1>
 			<MyButton />
+			<CommonGreeting />
 		</div>
 	);
 }
