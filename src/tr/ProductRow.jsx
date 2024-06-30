5a777ee3bab31cec3754711a6f8cef56e7e3e5c1
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-30 18:36:32 +0300

    update:ProductRow - put the selected product to the list

--- src/tr/ProductRow.jsx
+++ src/tr/ProductRow.jsx
@@ -1,11 +1,13 @@
-export default function ProductRow({ product }) {
+export default function ProductRow({ product, onSelectProduct }) {
 	const name = product.stocked
 		? product.name
 		: <span style={{ color: 'red' }}>{product.name}</span>;
 
 	return (
 		<tr>
-			<td>{name}</td>
+			<td onClick={() => onSelectProduct(product)}>
+				{name}
+			</td>
 			<td>{product.price}</td>
 		</tr>
 	);
