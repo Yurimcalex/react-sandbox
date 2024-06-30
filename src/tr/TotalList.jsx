9c334dd42d1bf109f186c265d58aff6812fc7839
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-30 19:10:10 +0300

    update:TotalList - show total sum

--- src/tr/TotalList.jsx
+++ src/tr/TotalList.jsx
@@ -14,15 +14,22 @@ export default function TotalList({ products }) {
 		<li key={product.name}>
 			<span>{product.name}</span>{' '}
 			<span>{product.amount}</span>{' '}
-			<span>${product.price.slice(1) * product.amount}</span>
+			<span>${calcPrice(product)}</span>
 		</li>
 	));
+
+	const totalSum = Object.values(list).reduce((sum, product) => sum += calcPrice(product), 0);
 
 	return (
 		<div>
 			<ul>
 				{listItems}
 			</ul>
+			<div>Total sum: ${totalSum}</div>
 		</div>
 	);
+}
+
+function calcPrice({ price, amount }) {
+	return price.slice(1) * amount;
 }
