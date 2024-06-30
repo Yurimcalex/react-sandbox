7b21703296f7b48893ad1c8bd51439fce67c93aa
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-30 18:32:07 +0300

    Feat:FilterableProductTable - use state to reflect selected products

--- src/tr/FilterableProductTable.jsx
+++ src/tr/FilterableProductTable.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react';
 import SearchBar from './SearchBar.jsx';
 import ProductTable from './ProductTable.jsx';
+import ShoppingCart from './ShoppingCart.jsx';
 
 export default function FilterableProductTable({ products }) {
 	const [filterText, setFilterText] = useState('');
 	const [isStockOnly, setIsStockOnly] = useState(false);
+
+	const [selectedProducts, setSelectedProducts] = useState([]);
+	const onSelectProduct = (product) => setSelectedProducts([...selectedProducts, product]); 
 
 	return (
 		<div>
@@ -18,7 +22,9 @@ export default function FilterableProductTable({ products }) {
 				products={products}
 				filterText={filterText}
 				isStockOnly={isStockOnly}
+				onSelectProduct={onSelectProduct}
 			/>
+			<ShoppingCart products={selectedProducts} />
 		</div>
 	);
 }
