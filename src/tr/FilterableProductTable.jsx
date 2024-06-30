3eef98b107e6e574bb0a2fa1fa742398e7c7f1cc
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-30 17:49:22 +0300

    update: FilterableProductTable - use state to process user input

--- src/tr/FilterableProductTable.jsx
+++ src/tr/FilterableProductTable.jsx
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import SearchBar from './SearchBar.jsx';
 import ProductTable from './ProductTable.jsx';
 
 export default function FilterableProductTable({ products }) {
+	const [filterText, setFilterText] = useState('');
+	const [isStockOnly, setIsStockOnly] = useState(false);
+
 	return (
 		<div>
-			<SearchBar />
-			<ProductTable products={products} />
+			<SearchBar 
+				filterText={filterText}
+				isStockOnly={isStockOnly}
+				onFilterTextChange={setFilterText}
+				onIsStockOnlyChange={setIsStockOnly}
+			/>
+			<ProductTable 
+				products={products}
+				filterText={filterText}
+				isStockOnly={isStockOnly}
+			/>
 		</div>
 	);
 }
