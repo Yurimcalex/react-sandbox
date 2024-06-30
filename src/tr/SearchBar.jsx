ae33f89c2a98fb05c5a9d2429682eafcfd8430aa
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-30 17:50:59 +0300

    update:SearchBar - use date from the parent

--- src/tr/SearchBar.jsx
+++ src/tr/SearchBar.jsx
@@ -1,10 +1,24 @@
-export default function SearchBar() {
+export default function SearchBar({ 
+	filterText,
+	isStockOnly,
+	onFilterTextChange,
+	onIsStockOnlyChange
+}) {
 	return (
 		<form>
-			<input type="text" placeholder="Search..." />
+			<input 
+				type="text"
+				placeholder="Search..."
+				value={filterText}
+				onChange={(e) => onFilterTextChange(e.target.value)}
+			/>
 			<br />
 			<label>
-				<input type="checkbox" />
+				<input 
+					type="checkbox"
+					checked={isStockOnly}
+					onChange={(e) => onIsStockOnlyChange(e.target.checked)}
+				/>
 				{' '}
 				Only show products in stock
 			</label>
