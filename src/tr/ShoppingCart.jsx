f58c1fb9bd5ffd8abaffd2e99cb33c6bfe5b6809
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-30 18:04:22 +0300

    Create Shopping cart static component

---
+++ src/tr/ShoppingCart.jsx
@@ -0,0 +1,14 @@
+export default function ShoppingCart() {
+	return (
+		<table>
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Amount</th>
+					<th>Price</th>
+				</tr>
+			</thead>
+			<tbody></tbody>
+		</table>
+	);
+}
