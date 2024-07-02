6126b0ba52203cb722e53e1c89069a150bd88715
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-02 16:30:18 +0300

    Update Animal component

--- src/du/Animal.jsx
+++ src/du/Animal.jsx
@@ -4,9 +4,9 @@ export default function Animal({ name, data }) {
 			<h3>My name is {name}, I am a {data.kind}</h3>
 			<p>
 				I can 
-				{data.skills.map(skill => <> <span>{skill}</span>, </>)}
+				{data.skills.map(skill => <em key={skill}> <span>{skill}</span>, </em>)}
 				I like
-				{data.food.map(item => <> <span>{item}</span>, </>)}
+				{data.food.map(item => <em key={item}> <span>{item}</span>, </em>)}
 				I am
 				{data.isFriendly ? ' friendly': ' not friendly'}
 				.
