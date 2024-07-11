590822c6ffe5c537b5c3a3a55ce1fc52b5cc9e4d
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-11 10:35:52 +0300

    Add show author feature for PhraseGallery component

--- src/ai/PhraseGallery.jsx
+++ src/ai/PhraseGallery.jsx
@@ -8,6 +8,7 @@ const data = [
 
 export default function PhraseGallery() {
 	const [ind, setInd] = useState(0);
+	const [showAuthor, setShowAuthor] = useState(false);
 
 	const handleClick = () => {
 		if (ind < data.length - 1) setInd(ind + 1);
@@ -18,6 +19,9 @@ export default function PhraseGallery() {
 			<p>{data[ind].text}</p>
 			<button onClick={handleClick}>Next</button>
 			<span>{ind + 1} of {data.length}</span>
+
+			<button onClick={() => setShowAuthor(!showAuthor)}>{showAuthor ? 'Hide': 'Show'} Author</button>
+			{showAuthor && <p>{data[ind].author}</p>}
 		</div>
 	);
 }
