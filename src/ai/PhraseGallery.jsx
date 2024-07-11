40a4cadd6c1d2103b925c2fe4e98f7e946f98f27
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-11 10:47:18 +0300

    feat: PhraseGallery - disable the next button when the data runs out

--- src/ai/PhraseGallery.jsx
+++ src/ai/PhraseGallery.jsx
@@ -10,14 +10,18 @@ export default function PhraseGallery() {
 	const [ind, setInd] = useState(0);
 	const [showAuthor, setShowAuthor] = useState(false);
 
-	const handleClick = () => {
-		if (ind < data.length - 1) setInd(ind + 1);
-	};
+	const handleClick = () => setInd(ind + 1);
 
 	return (
 		<div>
 			<p>{data[ind].text}</p>
-			<button onClick={handleClick}>Next</button>
+			<button 
+				onClick={handleClick}
+				disabled={ind === data.length - 1}
+			>
+				Next
+			</button>
+
 			<span>{ind + 1} of {data.length}</span>
 
 			<button onClick={() => setShowAuthor(!showAuthor)}>{showAuthor ? 'Hide': 'Show'} Author</button>
