6bd20ac87a489b884bba9fb156ca05c639b00323
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-11 14:47:10 +0300

    CounterWithInput - set the counter with input ability

--- src/ai/CounterWithInput.jsx
+++ src/ai/CounterWithInput.jsx
@@ -2,10 +2,22 @@ import { useState } from 'react';
 
 export default function CounterWithInput() {
 	const [counter, setCounter] = useState(0);
+	const [text, setText] = useState('');
+
+	const handleInputChange = (e) => {
+		const text = e.target.value;
+		setText(text);
+
+		if (parseInt(text)) {
+			setCounter(parseInt(text));
+		}
+	};
+
+	console.log('CounterWithInput rendered!');
 
 	return (
 		<div>
-			<input type="text" />
+			<input type="text" onChange={handleInputChange} />
 			<button onClick={() => setCounter(counter + 1)}>Increase {counter}</button>
 		</div>
 	);
