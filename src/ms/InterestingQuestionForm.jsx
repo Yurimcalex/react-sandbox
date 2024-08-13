ec5f55439930594cd70cc9093eca3a48981744de
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-08-13 17:52:39 +0300

    InterestingQuestionForm - create state and connect it to event handlers

--- src/ms/InterestingQuestionForm.jsx
+++ src/ms/InterestingQuestionForm.jsx
@@ -1,29 +1,38 @@
-const statuses = [
-	'empty',
-	'submitting',
-	'error',
-	'success',
-	'typing'
-];
-
+import { useState } from 'react';
 
 export default function InterestingQuestionForm() {
+	const [answer, setAnswer] = useState('');
+	const [error, setError] = useState(null);
+	const [status, setStatus] = useState('typing');
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+		setStatus('submitting');
+		try {
+			await submitForm(answer);
+			setStatus('success');
+		} catch (err) {
+			setStatus('typing');
+			setError(err);
+		}
+	};
+
 	return (
 		<div>
 			<h3>An interesting question</h3>
-			{statuses.map(status => (
-				<div>
-					<p>Form status={status}</p>
-					<Form status={status} />
-					<hr />
-				</div>
-			))}
+			<Form
+				status={status}
+				answer={answer}
+				setAnswer={setAnswer}
+				error={error}
+				submit={handleSubmit}
+			/>
 		</div>
 	);
 }
 
 
-function Form({ status = 'error' }) {
+function Form({ status, answer, setAnswer, error, submit }) {
 	if (status === 'success') {
 		return <h1>Great choice!</h1>
 	}
@@ -31,19 +40,35 @@ function Form({ status = 'error' }) {
 	return (
 		<div>
 			<h2>Do you want to get rich?</h2>
-			<form>
+			<form onSubmit={submit}>
 				<textarea 
 					disabled={status === 'submitting'}
+					value={answer}
+					onChange={(e) => setAnswer(e.target.value)}
 				/>
 				<br />
 				<button 
 					type="submit"
-					disabled={status === 'submitting' || status === 'empty'}
+					disabled={answer.length === 0 || status === 'submitting'}
 				>
 					Submit
 				</button>
-				{status === 'error' && <h2>It's only up to you to live your life!</h2>}
+				{error !== null && <h2>{error.message}</h2>}
 			</form>
 		</div>
 	);
+}
+
+
+function submitForm(answer) {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			let isError = answer.toLowerCase() !== 'yes';
+			if (isError) {
+				reject(new Error(`It's only up to you to live your life!`));
+			} else {
+				resolve();
+			}
+		}, 1500);
+	});
 }
