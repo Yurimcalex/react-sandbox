ad5fe020059ddd702d58668e0bd5f750a32e331b
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-08-15 09:07:15 +0300

    Create PlayerForm

--- src/ms/MS.jsx
+++ src/ms/MS.jsx
@@ -1,10 +1,11 @@
 import InterestingQuestionForm from './InterestingQuestionForm.jsx';
 import FeedbackForm from './FeedbackForm.jsx';
+import PlayerForm from './PlayerForm.jsx';
 
 export default function MS() {
 	return (
 		<div>
-			<FeedbackForm />
+			<PlayerForm />
 		</div>
 	);
 }
