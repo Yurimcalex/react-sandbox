8071e56b4112578c81ed33f5f31d7770b7fa8336
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-08-15 14:04:15 +0300

    update:SmallChat - disable input when sending

--- src/ms/SmallChat.jsx
+++ src/ms/SmallChat.jsx
@@ -2,44 +2,65 @@ import { useState } from 'react';
 
 export default function SmallChat() {
 	const [recipient, setRecipient] = useState('John');
+	const [status, setStatus] = useState('typing');
+	const [message, setMessage] = useState('');
 
 	const handleSubmit = async (recipient, message, e) => {
 		e.preventDefault();
 		try {
+			setStatus('sending');
 			const responce = await sendMessage(recipient, message);
 			console.log(responce);
+			setStatus('sent');
 		} catch (err) {
 			console.log(err);
 		}
 	};
 
-	const handleRecipientChange = (name) => setRecipient(name);
+	const handleMessageChange = (e) => {
+		setMessage(e.target.value);
+		setStatus('typing');
+	};
+
+	const handleRecipientChange = (name) => {
+		setRecipient(name);
+		setMessage('');
+	};
 
 	return (
 		<div>
 			<h1>Recipient: {recipient}</h1>
 			<div>
-				<Form key={recipient} recipient={recipient} onSend={handleSubmit} />
+				<Form 
+					key={recipient}
+					status={status}
+					recipient={recipient}
+					message={message}
+					onSend={handleSubmit}
+					onMessageChange={handleMessageChange} />
 			</div>
 			<br />
 			<div>
-				<button onClick={() => handleRecipientChange('John')}>send to John</button>
-				<button onClick={() => handleRecipientChange('Alice')}>send to Alice</button>
-				<button onClick={() => handleRecipientChange('Michael')}>send to Michael</button>
+				<button onClick={() => handleRecipientChange('John')} disabled={status === 'sending'}>send to John</button>
+				<button onClick={() => handleRecipientChange('Alice')} disabled={status === 'sending'}>send to Alice</button>
+				<button onClick={() => handleRecipientChange('Michael')} disabled={status === 'sending'}>send to Michael</button>
 			</div>
+			{status === 'sent' && <div>message: {message} is delivered to - {recipient}</div>}
 		</div>
 	);
 }
 
 
-function Form({ recipient, onSend }) {
-	const [message, setMessage] = useState('');
-
+function Form({ recipient, message, status, onSend, onMessageChange }) {
 	return (
 		<form key={recipient} onSubmit={(e) => onSend(recipient, message, e)}>
-			<textarea value={message} onChange={(e) => setMessage(e.target.value)} />
+			<textarea 
+				value={message}
+				onChange={onMessageChange}
+				disabled={status === 'sending'}
+			/>
 			<br />
-			<button type="submit">Send</button>
+			<button type="submit" disabled={status === 'sending'}>Send</button>
 		</form>
 	);
 }
