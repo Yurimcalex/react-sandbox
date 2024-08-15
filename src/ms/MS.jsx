e6cd45448faa3cd73d2cd53ec883727885becdf1
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-08-15 09:38:49 +0300

    Create TravelSnackSurvey component

--- src/ms/MS.jsx
+++ src/ms/MS.jsx
@@ -2,11 +2,12 @@ import InterestingQuestionForm from './InterestingQuestionForm.jsx';
 import FeedbackForm from './FeedbackForm.jsx';
 import PlayerForm from './PlayerForm.jsx';
 import InfoPanel from './InfoPanel.jsx';
+import TravelSnackSurvey from './TravelSnackSurvey.jsx';
 
 export default function MS() {
 	return (
 		<div>
-			<InfoPanel />
+			<TravelSnackSurvey />
 		</div>
 	);
 }
