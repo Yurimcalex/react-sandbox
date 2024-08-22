fd09d20d1f1eb95ca7737daa54b5110167be3329
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-08-22 13:36:20 +0300

    Tasks: edit task func

--- src/ms/Tasks.jsx
+++ src/ms/Tasks.jsx
@@ -17,27 +17,49 @@ export default function Tasks() {
 
 	const handleTaskDelete = (id) => setTasks(tasks.filter(task => task.id !== id));
 
+	const handleTaskEdit = (id, text) => {
+		setTasks(tasks.map((task) => {
+			if (task.id === id) {
+				return { ...tasks, text };
+			}
+			return task;
+		}));
+	};
+
 	return (
 		<div>
 			<h1>Tasks</h1>
 			<AddTask onTaskAdd={handleTaskAdd} />
-			<TaskList tasks={tasks} onTaskDelete={handleTaskDelete} />
+			<TaskList tasks={tasks} handleTaskDelete={handleTaskDelete} handleTaskEdit={handleTaskEdit} />
 		</div>
 	);
 }
 
 
-function TaskList({ tasks, onTaskDelete }) {
+function Task({ task, onTaskDelete, onTaskEdit }) {
+	const [isEditing, setIsEditing] = useState(false);
+	const [text, setText] = useState('');
+
 	return (
 		<div>
-			{tasks.map(task => (
-				<div key={task.id}>
-					<input type="checkbox" checked={task.done} />
-					{task.text}
-					<button>Edit</button>
-					<button onClick={() => onTaskDelete(task.id)}>Delele</button>
-				</div>
-			))}
+			<input type="checkbox" checked={task.done} />
+
+			{isEditing ? <input type="text" onChange={(e) => setText(e.target.value)} /> : task.text}
+
+			{isEditing
+				? <button onClick={() => onTaskEdit(task.id, text)}>Save</button>
+				: <button onClick={() => setIsEditing(!isEditing)}>Edit</button>}
+			
+			<button onClick={() => onTaskDelete(task.id)}>Delele</button>
+		</div>
+	);
+}
+
+
+function TaskList({ tasks, handleTaskDelete, handleTaskEdit }) {
+	return (
+		<div>
+			{tasks.map(task => <Task key={task.id} task={task} onTaskDelete={handleTaskDelete} onTaskEdit={handleTaskEdit} />)}
 		</div>
 	);
 }
